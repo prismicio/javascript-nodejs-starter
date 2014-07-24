246e3734df376972949dbd26c2651ca90f35c8c7
Guillaume Grossetie <g.grossetie@gmail.com>
2014-07-24 19:34:36 +0200

    Update to Express 4.6.1

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express'),
+    favicon = require('static-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    session = require('express-session'),
+    errorHandler = require('errorhandler'),
     routes = require('./routes'),
     http = require('http'),
     path = require('path'),
@@ -17,29 +23,29 @@ app.engine('ejs', engine);
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('1234'));
-app.use(express.session());
-app.use(app.router);
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(cookieParser('1234'));
+app.use(session({secret: '1234', saveUninitialized: true, resave: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.errorHandler);
+app.use(errorHandler());
 
 // Routes
-app.get('/', routes.index);
-app.get('/documents/:id/:slug', routes.detail);
-app.get('/search', routes.search);
+app.route('/').get(routes.index);
+app.route('/documents/:id/:slug').get(routes.detail);
+app.route('/search').get(routes.search);
 
 // OAuth - Routes
-app.get('/signin', prismic.signin);
-app.get('/auth_callback', prismic.authCallback);
-app.post('/signout', prismic.signout);
+app.route('/signin').get(prismic.signin);
+app.route('/auth_callback').get(prismic.authCallback);
+app.route('/signout').post(prismic.signout);
 
-var PORT = process.env.PORT || 3000;
+var PORT = app.get('port');
 
-http.createServer(app).listen(PORT, function() {
+app.listen(PORT, function() {
   console.log('Express server listening on port ' + PORT);
 });
+
