06f670b4554d40204593c108db48f59363ac4a85
Erwan Loisant <eloisant@gmail.com>
2014-12-16 14:27:42 +0100

    Update kit version

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,6 +1,6 @@
 exports.Configuration = {
 
-  apiEndpoint: 'https://lesbonneschoses-vcerzcwaaohojzo.prismic.io/api',
+  apiEndpoint: 'https://lesbonneschoses.prismic.io/api',
 
   // -- Access token if the Master is not open
   // accessToken: 'xxxxxx',
